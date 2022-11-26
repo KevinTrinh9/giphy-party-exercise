86767356c95ad837587113d3c80178b3b7d9f76d
Kevin Trinh <kevintrinh@Computer.local>
2022-11-25 21:50:04 -0800

    add files

---
+++ app.js
@@ -0,0 +1,27 @@
+let $gifArea = $("#gif-area")
+let $searchInput = $("#search")
+function addGif(result) {
+    let numResults = result.data.length
+    if (numResults) {
+        let randomIdx = Math.floor(Math.random() * numResults)
+        let $newCol = $("<div>", {class: "col-md-4 col-12 mb-4"})
+        let $newGif = $("<img>", {src: result.data[randomIdx].images.original.url, class: "w-100"})
+        $newCol.append($newGif)
+        $gifArea.append($newCol)
+    }
+}
+$("form").on("submit", async function(event) {
+    event.preventDefault()
+    let searchTerm = $searchInput.val()
+    $searchInput.val("")
+    let response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+        params: {
+            q: searchTerm,
+            api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+        }
+    })
+    addGif(response.data)
+})
+$("#remove").on("click", function() {
+    $gifArea.empty()
+})
